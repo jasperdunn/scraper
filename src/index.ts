cbb979c13ead51051e1cf61c08758d51a5642bde
Jasper Dunn <jasperdunn93@gmail.com>
2022-03-29 21:35:01 +1000

    implement example scraper

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-console.log('Hello world!')
+import { getPosts } from './jasperdunn'
+import { webkit } from 'playwright'
+import chalk from 'chalk'
+import ora from 'ora'
 
-export {}
+async function scrape(): Promise<void> {
+  const launchingBrowser = ora('Launching browser').start()
+  const browser = await webkit.launch()
+  launchingBrowser.succeed(chalk.green('Browser is launched'))
+
+  const retrievingPosts = ora('Retrieving posts').start()
+  const posts = await getPosts(browser)
+  retrievingPosts.succeed(chalk.green('Posts were retrieved'))
+  console.log(chalk.grey(JSON.stringify(posts, null, 2)))
+
+  await browser.close()
+  console.log('Closed browser')
+}
+
+scrape()
